6d5d76539a834c05c11a15d715b13bd0eee508bd
Shamridha <147306405+Shamridha@users.noreply.github.com>
2024-09-19 17:23:50 +0530

    Time: 2 ms (54.64%), Space: 43.5 MB (18.86%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<String> al=new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        generate(n,0,0,"");
+        return al;
+    }
+    public void generate(int n,int c,int o,String ans)
+    {
+        if(c==n&&o==n)
+        {
+            al.add(ans);
+            return;
+        }
+        if(o>c||c>n||o>n)
+        return;
+        generate(n,c+1,o,ans+"(");
+        generate(n,c,o+1,ans+")");
+    }
+}
